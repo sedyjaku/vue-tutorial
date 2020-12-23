0dc978bcddb4e7d9f4fcfb8c62005c26ccb5df75
Jakub Šedý <jakubsedyy@gmail.com>
2020-12-23 20:08:10 +0100

    Add form with prevent default

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -6,6 +6,10 @@ const app = Vue.createApp({
     };
   },
   methods: {
+    submitForm(event){
+      event.preventDefault();
+      alert('Submitted!');
+    },
     reduce(num){
       this.counter+=num
     },
