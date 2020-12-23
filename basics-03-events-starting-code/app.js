ddbb30e99d63387dd91bc0cb3d296f394536ad6b
Jakub Šedý <jakubsedyy@gmail.com>
2020-12-23 19:59:19 +0100

    Add name input and print

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,11 +2,15 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
   },
   methods: {
     reduce(num){
       this.counter+=num
+    },
+    setName(event){
+      this.name =  event.target.value
     }
   }
 });
