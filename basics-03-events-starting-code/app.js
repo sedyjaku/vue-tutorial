a3e7f7d32eecb3e3be8093d0f5014f08740c8fdf
Jakub Šedý <jakubsedyy@gmail.com>
2020-12-24 07:49:40 +0100

    Add two way binding

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,8 +2,28 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      fullNameData: ''
     };
+  },
+  computed: {
+    fullname() {
+      if (this.name === ''){
+        return '';
+      }
+      return this.name + ' Lastname'
+    }
+  },
+  watch: {
+    name(value) {
+      if (value ===''){
+        this.fullNameData = ''
+      }
+      else {
+        this.fullNameData = this.name + ' Lastname'
+      }
+      console.log("name changed")
+    }
   },
   methods: {
     submitForm(event){
@@ -15,6 +35,15 @@ const app = Vue.createApp({
     },
     setName(event){
       this.name =  event.target.value
+    },
+    resetInput(){
+      this.name = ''
+    },
+    outputFullName(){
+      if (this.name === ''){
+        return '';
+      }
+      return this.name + ' Lastname'
     }
   }
 });
