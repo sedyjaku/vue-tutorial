806e711f6a3ff4574f4473bb9a67e5aed492cd52
Jakub Šedý <jakubsedyy@gmail.com>
2020-12-26 11:20:08 +0100

    Add friend-contact component

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{detailsAreVisible ? 'Show' : 'Hide'}} Details</button>
+    <ul v-show="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "friend-contact",
+  data() {
+    return {
+      detailsAreVisible: false,
+    }
+  },
+  props: {
+    friend: Object
+  },
+  methods: {
+    toggleDetails(){
+      this.detailsAreVisible = !this.detailsAreVisible
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-cli-01-a-new-vue-project/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue';
+import App from "@/App";
+import FriendContact from "@/components/FriendContact";
+
+
+const app = createApp(App);
+
+app.component('friend-contact', FriendContact);
+
+app.mount('#app')
