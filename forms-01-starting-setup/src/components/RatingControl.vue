e511105b95a69966bcc14cd1f6eaa6628d0aae4d
Jakub Šedý <jakubsedyy@gmail.com>
2020-12-27 14:22:35 +0100

    Add form controlls

---
+++ forms-01-starting-setup/src/components/RatingControl.vue
@@ -0,0 +1,55 @@
+<template>
+  <ul>
+    <li :class="{active: activeOption==='poor'}"><button type="button" @click="activate('poor')">Poor</button></li>
+    <li :class="{active: activeOption==='average'}"><button type="button" @click="activate('average')" >Average</button></li>
+    <li :class="{active: activeOption==='great'}"><button type="button" @click="activate('great')">Great</button></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'RatingControl',
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  computed: {
+    activeOption(){
+      return this.modelValue
+    }
+  },
+  methods: {
+    activate(option){
+      this.activeOption = option;
+      this.$emit('update:modelValue', option);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: #7811d7
+}
+
+ul{
+  list-style:none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button{
+  font:inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
